fe1d8d7a8605b6dd7cd350d7181dd573b690dfc7
Kkkeksikkk753 <159584307+Zubin753@users.noreply.github.com>
2024-10-02 02:22:46 +0300

    Delete test-crypt directory

--- test-crypt/src/shared/index.ts
+++
@@ -1,7 +0,0 @@
-import {InputVariants} from "./ui/Input";
-
-export {Button} from "./ui/Button"
-export {ButtonVariants} from "./ui/Button"
-export {Input} from "./ui/Input"
-export {InputVariants} from "./ui/Input"
-export {Logo} from "./ui/Logo"
